bd3679e515801939d9fce6f2ee97f7b9bf24d778
john-git7 <gideonebenezer467@gmail.com>
2025-03-05 13:48:20 +0530

    done

---
+++ vite-project/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>John Ebenezer</h1>
+      <p>blazer69@gmail.com</p>
+    </div>
+  );
+};
+
+export default Home;
